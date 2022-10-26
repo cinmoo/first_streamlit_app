9d607f1be6409dad01065076b0f28c93afc0f9b6
Cindy Moore <116734391+cinmoo@users.noreply.github.com>
2022-10-26 08:57:36 -0500

    Update streamlit_app.py
    added Emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smootie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('👌 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🤢 Kale, Spinach & Rocket Smootie')
+streamlit.text('🤢🤢 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
